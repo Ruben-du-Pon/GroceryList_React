be197f0ee895fbcfe2c6ca638a9035ecab84e5a0
Ruben-du-Pon <rubenmdupon@gmail.com>
2025-10-08 11:44:37 +0200

    main structure

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DefaultList from "./DefaultList"
+import List from "./List"
+
+const App = () => {
+    return(
+        <>
+            <DefaultList />
+            <List />
+        </>
+    )
+}
